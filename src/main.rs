dd0127cc5beb06e918f33221c01e541e342d4089
enomoto11 <t.enomoto@catallaxy.me>
2024-01-07 01:03:15 +0900

    HttpServerのbind部分を改修

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+use std::io::Error;
+use std::net::Ipv4Addr;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let port = get_port().unwrap();
+
+    return HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((localhost, port))?
     .run()
-    .await
+    .await;
+}
+
+fn get_port() -> Result<u16, Error> {
+    let port = env::var("PORT");
+    match port {
+        Ok(p) => Ok(p.parse::<u16>().unwrap()),
+        Err(_) => Err(Error::new(std::io::ErrorKind::Other, "No port")),
+    }
 }
 
 #[get("/")]

4febf6be85e799e7af0c62fda97c74cb0bb3152b
enomoto11 <t.enomoto@catallaxy.me>
2024-01-08 21:04:22 +0900

    別ファイルに切り出した

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+use std::net::Ipv4Addr;
+
+pub struct Server {
+    host: Ipv4Addr,
+    port: u16,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        // FIXME: 上位環境ごとに設定を変える
+        let localhost: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+        let port: u16 = 8080;
+
+        return Self {
+            host: localhost,
+            port,
+        };
+    }
+
+    pub fn get_host(&self) -> Ipv4Addr {
+        return self.host;
+    }
+
+    pub fn get_port(&self) -> u16 {
+        return self.port;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use actix_web::{web, App, HttpServer};
+mod config;
+mod controller;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let config = server_config::new();
+    let config = config::Server::new();
 
     return HttpServer::new(|| {
         App::new()
@@ -10,55 +12,7 @@ async fn main() -> std::io::Result<()> {
             .service(controller::echo)
             .route("/hey", web::get().to(controller::manual_hello))
     })
-    .bind((config.host, config.port))?
+    .bind((config.get_host(), config.get_port()))?
     .run()
     .await;
-}
-
-mod server_config {
-    use dotenv::dotenv;
-    use std::env;
-    use std::io::Error;
-    use std::net::Ipv4Addr;
-
-    pub struct ServerConfig {
-        pub host: Ipv4Addr,
-        pub port: u16,
-    }
-
-    pub fn new() -> ServerConfig {
-        dotenv().ok();
-        // FIXME: 上位環境ごとに設定を変える
-        let localhost: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
-        let port = get_port().unwrap();
-        return ServerConfig {
-            host: localhost,
-            port,
-        };
-    }
-
-    fn get_port() -> Result<u16, Error> {
-        let port = env::var("PORT");
-        match port {
-            Ok(p) => Ok(p.parse::<u16>().unwrap()),
-            Err(_) => Err(Error::new(std::io::ErrorKind::Other, "No port")),
-        }
-    }
-}
-
-mod controller {
-    use actix_web::{get, post, HttpResponse, Responder};
-    #[get("/")]
-    pub async fn hello() -> impl Responder {
-        HttpResponse::Ok().body("Hello world!")
-    }
-
-    #[post("/echo")]
-    pub async fn echo(req_body: String) -> impl Responder {
-        HttpResponse::Ok().body(req_body)
-    }
-
-    pub async fn manual_hello() -> impl Responder {
-        HttpResponse::Ok().body("Hey there!")
-    }
 }
